831db79ebab92adeecdb63a28143b189c1f50646
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-01-19 19:18:37 +0100

    feat: add SettingsBloc to MultiBlocProvider for settings management

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nebula/drawer.dart';
 import 'package:nebula/navigation/bloc/navigation_bloc.dart';
+import 'package:nebula/settings/bloc/settings_bloc.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,6 +12,7 @@ class App extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => NavigationBloc()),
+        BlocProvider(create: (_) => SettingsBloc()),
       ],
       child: MaterialApp(
         title: 'Nebula',
