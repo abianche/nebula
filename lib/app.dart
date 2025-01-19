b4e1a6256df045a5424cdd628c049fd940e27e20
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-01-19 21:27:15 +0100

    feat: implement dynamic theme switching based on app settings

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nebula/drawer.dart';
+import 'package:nebula/models/settings_model.dart';
 import 'package:nebula/navigation/bloc/navigation_bloc.dart';
 import 'package:nebula/settings/bloc/settings_bloc.dart';
 
@@ -14,9 +15,27 @@ class App extends StatelessWidget {
         BlocProvider(create: (_) => NavigationBloc()),
         BlocProvider(create: (_) => SettingsBloc()),
       ],
-      child: MaterialApp(
-        title: 'Nebula',
-        home: const Navigation(),
+      child: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, state) {
+          getThemeMode() {
+            switch (state.settings.appTheme) {
+              case AppTheme.light:
+                return ThemeMode.light;
+              case AppTheme.dark:
+                return ThemeMode.dark;
+              default:
+                return ThemeMode.system;
+            }
+          }
+
+          return MaterialApp(
+            title: 'Nebula',
+            home: const Navigation(),
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            themeMode: getThemeMode(),
+          );
+        },
       ),
     );
   }
