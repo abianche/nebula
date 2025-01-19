c3828216953f5de39f4cc609f33e2531c7fc8f6f
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2025-01-19 19:07:18 +0100

    feat: create App widget with MultiBlocProvider for navigation management

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nebula/drawer.dart';
+import 'package:nebula/navigation/bloc/navigation_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => NavigationBloc()),
+      ],
+      child: MaterialApp(
+        title: 'Nebula',
+        home: const Navigation(),
+      ),
+    );
+  }
+}
